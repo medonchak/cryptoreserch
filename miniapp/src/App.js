a0e06f37faae3eec81e03f28f2bf9eb702e042ac
John Doe <archichak999@email.com>
2024-06-09 12:07:05 +0300

    test3

--- miniapp/src/App.js
+++ miniapp/src/App.js
@@ -3,12 +3,13 @@ import './App.css';
 
 function App() {
   const tg = window.Telegram.WebApp
+  const test =  tg.initDataUnsafe.user
   return (
     <div className="App">
       <header className="App-header">
       
-       
-              {tg.initDataUnsafe.user.username}
+      {test}
+             
        
         
       </header>
