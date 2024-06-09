9aef9759fb80f342966a43c6d0d45918914b8dbb
John Doe <archichak999@email.com>
2024-06-09 11:21:17 +0300

    test

--- miniapp/src/App.js
+++ miniapp/src/App.js
@@ -2,12 +2,13 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const tg = window.Telegram.WebApp
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+              {tg.initDataUnsafe.user.username}
         </p>
         <a
           className="App-link"
